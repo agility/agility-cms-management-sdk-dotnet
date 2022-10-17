57c91592c34ac3d4887dccfecce6fe8b1572cdea
Mohit Vashishtha <mohit.vashishtha6284@gmail.com>
2022-10-17 16:19:10 -0400

    Refresh Token logic

---
+++ management.api.sdk/AuthMethods.cs
@@ -0,0 +1,53 @@
+﻿using agility.models;
+using RestSharp;
+using System.Dynamic;
+using System.Text;
+using System.Text.Json;
+
+namespace management.api.sdk
+{
+    public class AuthMethods
+    {
+        private Options _options = null;
+        ClientInstance _clientInstance = null;
+        public AuthMethods(Options options)
+        {
+            _options = options;
+            _clientInstance = new ClientInstance();
+        }
+
+        public async Task<string> GetAuthorizationToken(string? guid, string? path)
+        {
+
+            var baseURL = _options.DetermineBaseForAuth(guid);
+            var client = new RestClient($"{baseURL}/oauth/refresh");
+            client.AddDefaultHeader("Cache-Control", "no-cache");
+
+            var request = new RestRequest($"{baseURL}/oauth/refresh");
+            request.Method = Method.Post;
+            request.AddParameter("refresh_token", _options.refresh_token, ParameterType.RequestBody);
+            var response = client.ExecuteAsync(request);
+            var resp = response.Result.Content;
+            ExportToken(resp, path);
+            return resp;
+        }
+
+        private void ExportToken(string? content, string? path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                File.WriteAllText(path, content);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
--- management.api.sdk/models/Options.cs
+++ management.api.sdk/models/Options.cs
@@ -7,6 +7,8 @@
         public string? baseUrl { get; set; }
         public string? guid { get; set; }
 
+        public string? refresh_token { get; set; }
+
         public string DetermineBaseURL(string guid)
         {
             if (guid.EndsWith("-d"))
@@ -23,7 +25,7 @@
             }
             else if (guid.EndsWith("-eu"))
             {
-                baseUrl = "https://mgmt-eu.aglty.io/api/v1/instanceo";
+                baseUrl = "https://mgmt-eu.aglty.io/api/v1/instance";
             }
             else if (guid.EndsWith("-aus"))
             {
@@ -34,6 +36,34 @@
                 baseUrl = "https://mgmt-us.aglty.io/";
             }
             return baseUrl;
+        }
+
+        public string DetermineBaseForAuth(string guid)
+        {
+            if (guid.EndsWith("-d"))
+            {
+                return "https://mgmt-dev.aglty.io";
+            }
+            else if (guid.EndsWith("-u"))
+            {
+                return "https://mgmt-us.aglty.io";
+            }
+            else if (guid.EndsWith("-ca"))
+            {
+                return "https://mgmt-ca.aglty.io";
+            }
+            else if (guid.EndsWith("-eu"))
+            {
+                return "https://mgmt-eu.aglty.io";
+            }
+            else if (guid.EndsWith("-aus"))
+            {
+                return "https://mgmt-aus.aglty.io";
+            }
+            else
+            {
+                return "https://mgmt-us.aglty.io/";
+            }
         }
     }
 }
